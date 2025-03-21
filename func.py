638652403327fb72be49b022c0757f2a2a318c60
yonioren <why@shiz.shiz>
2025-03-21 10:47:38 +0200

    * Allowed multiple expenses and incomes * Some validations? maybe?

--- func.py
+++ func.py
@@ -4,24 +4,79 @@ expenses = []
 history = {'his':[]}
 balance = 0
 
+### Interactive functions ###
+def get_valid_float(prompt):
+    while True:
+        try:
+            if prompt and not prompt.strip()[-1] == ":":
+                prompt=prompt.strip()+": "
+            value = float(input(prompt))
+
+            if value > 0:
+                return value
+            else:
+                print("Please enter a value greater than 0.\n")
+        except ValueError:
+            print("Invalid input, please enter a valid number.\n")
+
+def get_valid_string(prompt):
+    if prompt and not prompt.strip()[-1] == ":":
+        prompt = prompt.strip() + ": "
+
+    while True:
+        value = input(prompt).strip()
+
+        if value:
+            return value
+        else:
+            print("Invalid input, the string cannot be empty.\n")
+
+def get_valid_y_n(prompt):
+    if prompt:
+        prompt=prompt.strip()+" (Y/n): "
+    value = get_valid_string(prompt)
+
+    while True:
+        if value.lower() not in ['n','y']:
+            value = get_valid_string("Y/n? ")
+        else:
+            break
+
+    return value.lower() == 'y'
 
 #increase balance and append to history
-def increase(price,desc):
+def increase():
     global balance
-    indata = {"price":price,"desc":desc,"type":'incomes'}
-    history['his'].append(indata)
-    balance = balance + price
+    while True:
+        price = get_valid_float('add income number')
+        desc = get_valid_string('description')
+        indata = {"price":price,"desc":desc,"type":'incomes'}
+        history['his'].append(indata)
+        balance = balance + price
+
+        if not get_valid_y_n("Do you want to add another income?"):
+            return
+
+
 #decrease balance and append to history
-def decrease(price,desc):
-    global balance
-    indata = {"price":price,"desc":desc,"type":'expenses'}
-    print(indata['price'])
-    history['his'].append(indata)
-    balance = balance-price
+def decrease():
+    while True:
+        global balance
+        price = get_valid_float('add expense number')
+        desc = get_valid_string('description')
+        indata = {"price":price,"desc":desc,"type":'expenses'}
+
+        history['his'].append(indata)
+        balance = balance-price
+
+        if not get_valid_y_n("Do you want to add another expense?"):
+            return
+
 #show current balance
 def show_balance():
     global balance
     print(balance)
+
 #show transaction_history
 def transaction_history():
     print('No','price','description','type')
